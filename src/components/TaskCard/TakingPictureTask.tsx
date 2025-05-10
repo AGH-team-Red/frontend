6f43774de2a61f28057e480b8823da4b808887a3
Dominik Sieroń <71605737+CALLmeDOMIN@users.noreply.github.com>
2025-05-11 00:14:38 +0200

    Fix: fixup taking picture task

--- src/components/TaskCard/TakingPictureTask.tsx
+++ src/components/TaskCard/TakingPictureTask.tsx
@@ -1,9 +1,13 @@
+'use client';
+
 import { IconTooltip } from '@/components/IconTooltip';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent } from '@/components/ui/card';
 import { Citrus } from 'lucide-react';
 import Image from 'next/image';
 import { PictureTask } from '@/lib/types';
+import { UploadDropzone } from '@/lib/uploadthing';
+import { useState } from 'react';
 
 export default function TakingPictureTask({ pictureTask }: { pictureTask?: PictureTask }): React.ReactElement {
   // TODO: Rethink typing props
@@ -11,7 +15,11 @@ export default function TakingPictureTask({ pictureTask }: { pictureTask?: Pictu
     return <div>Could not load picture task</div>;
   }
 
-  const { exampleImgUrl } = pictureTask;
+  const [uploadedImage, setUploadedImage] = useState('');
+  const [isUploading, setIsUploading] = useState(false);
+  // const { exampleImgUrl } = pictureTask;
+
+  console.log(uploadedImage);
 
   return (
     <>
@@ -20,7 +28,7 @@ export default function TakingPictureTask({ pictureTask }: { pictureTask?: Pictu
           <h2>Example image</h2>
           <div className="relative aspect-[16/9] w-full">
             <Image
-              src={exampleImgUrl || ''}
+              src={'https://picsum.photos/350/160'} // TODO ADD REAL EXAMPLE IMAGE
               className="rounded-md object-cover"
               fill
               sizes="(max-width: 768px) 100vw, 700px"
@@ -41,10 +49,32 @@ export default function TakingPictureTask({ pictureTask }: { pictureTask?: Pictu
         </IconTooltip>
         Upload Image
       </div>
-      <div className="flex h-20 w-full items-center justify-center rounded-md bg-gray-600/20">
-        Placeholder for image uploader
-      </div>
-      <Button>Save Image</Button>
+      <UploadDropzone
+        disabled={isUploading || uploadedImage !== ''}
+        endpoint="imageUploader"
+        appearance={{
+          button: 'ut-uploading:cursor-not-allowed bg-white text-black border-white shadow-sm hover:bg-white/90',
+          allowedContent: 'text-white'
+        }}
+        className="bg-foreground/10 ut-button:bg-foreground ut-button:text-background ut-allowed-content:text-foreground ut-label:text-foreground/50 hover:ut-label:text-foreground ut-uploading:ut-button:cursor-not-allowed ut-uploading:ut-button:bg-foreground/90"
+        onUploadBegin={() => {
+          setIsUploading(true);
+        }}
+        onClientUploadComplete={(res) => {
+          setUploadedImage(res[0].ufsUrl);
+          setIsUploading(false);
+        }}
+        onUploadError={(error: Error) => {
+          console.error('Upload error:', error); // TODO MAYBE ADD TOAST
+          setIsUploading(false);
+        }}
+      />
+      <Button
+        disabled={isUploading || !uploadedImage || uploadedImage === ''}
+        onClick={() => console.log('Image saved:', uploadedImage)} // TODO: SEND IMAGE TO BACKEND
+      >
+        Save Image
+      </Button>
     </>
   );
 }
