85a68d23b61f01076c4a5dedc85a37a0f5418150
Dominik Sieroń <71605737+CALLmeDOMIN@users.noreply.github.com>
2025-05-15 14:21:51 +0200

    Feat: Browse order desktop design and mobile changes

--- src/components/DetailsHeader.tsx
+++ src/components/DetailsHeader.tsx
@@ -1,50 +1,45 @@
 import ProgressCircle from '@/components/ProgressCircle';
-import { Card, CardContent } from '@/components/ui/card';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 
-type TRequestHeaderData = {
+type RequestHeaderData = {
   label: string;
   value: string;
 };
 
-type TProgressCircleData = {
+type ProgressCircleData = {
   samplesCurrent: number;
   samplesTotal: number;
 };
 
-type TDetailsHeaderProps = {
-  datasetName: string;
-  requestDescrption: string;
-  progressCircleData: TProgressCircleData;
-  requestHeaderData: TRequestHeaderData[];
+type DetailsHeaderProps = {
+  orderDescription: string;
+  progressCircleData: ProgressCircleData;
+  orderHeaderData: RequestHeaderData[];
 };
 
-const DetailsHeader = ({
-  datasetName,
-  requestDescrption,
-  progressCircleData,
-  requestHeaderData
-}: TDetailsHeaderProps) => {
+const DetailsHeader = ({ orderDescription, progressCircleData, orderHeaderData }: DetailsHeaderProps) => {
   return (
     <Card className="p-6">
       <CardContent className="flex flex-col gap-3 p-0">
-        <div className="flex gap-2">
-          <div className="flex items-center pr-4">
+        <CardHeader className="p-0 pb-2">
+          <CardTitle className="md:text-2xl">Challenge Overview</CardTitle>
+        </CardHeader>
+        <div className="flex items-center gap-2">
+          <div className="mr-4 flex items-center">
             <ProgressCircle current={progressCircleData.samplesCurrent} total={progressCircleData.samplesTotal} />
           </div>
-          <div className="flex flex-1 flex-col space-y-2">
-            <h2 className="text-md font-medium">{datasetName}</h2>
-            <div className="flex gap-3">
-              {requestHeaderData.map((item) => (
-                <div key={item.value} className="flex flex-col">
-                  <span className="text-xs">{item.label}</span>
-                  <span className="text-2xl">{item.value}</span>
-                </div>
-              ))}
-            </div>
+          <div className="flex w-full justify-between">
+            {orderHeaderData.map((item) => (
+              <div key={item.value} className="flex flex-col">
+                <div className="text-xs text-gray-400 sm:text-base">{item.label}</div>
+                <div className="text-xl font-bold sm:text-3xl">{item.value}</div>
+              </div>
+            ))}
           </div>
         </div>
-        <div>
-          <p className="text-sm">{requestDescrption}</p>
+        <div className="border-t border-gray-800 pt-4">
+          <h3 className="mb-3 text-xl font-semibold">Description</h3>
+          <p className="leading-relaxed text-gray-300">{orderDescription}</p>
         </div>
       </CardContent>
     </Card>
